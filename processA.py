2b02a3810ad7abe5f15b8b36779faaa6a4c8f707
jaylan <jaylan@gitlab.cs470.local>
2022-07-20 15:00:17 -0700

    checking if outputs are right

--- processA.py
+++ processA.py
@@ -1,7 +1,11 @@
 import zmq
 from time import sleep
-from stl import mesh
-import socket as netsocket
+# from stl import mesh
+import stl
+import socket
+import tqdm
+import os
+import sys
 
 #Sending
 #Setting up ZeroMQ publisher
@@ -9,57 +13,75 @@ context = zmq.Context()
 # sender = context.socket(zmq.PUB)
 # sender.bind('tcp://127.0.0.1:5555')
 
-file = mesh.Mesh.from_file('cad_mesh.stl')
+file = stl.mesh.Mesh.from_file('cad_mesh.stl')
 print("Sending stl file...")
 
 sender = context.socket(zmq.REQ)
 sender.connect('tcp://127.0.0.1:5555')
 #################################################
 
-#Recieving
-# s = netsocket.socket()
-# host = input(str("Enter host address:"))
-# port = 5555
-# s.connect((host, port))
-
-# print("Connected!")
-
-# print("Waiting to recieve file...")
-################
+#Sending file to process b and waiting for a
+#response before moving on
 arrived = None
 while not arrived:
     sender.send_pyobj(file)
     arrived = sender.recv_pyobj()
 
-print(type(arrived))
-#############
-# recieved = None
-# while(not recieved):
-#     sender.send_pyobj(file)
-#     try:
-#         reciever.send_string("Confirmed")
-#     except:
-#         sleep(1) 
-#         try:
-#             print("got it")
-#             recieved = reciever.rcvmore()
-#             print('MMMMMM')
-#         except:
-#             print("No confirmation of receipt")
+print("Recieved confimation!")
+sender.close()
+
+#Recieving
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind((socket.gethostname(), 5556))
+# s.listen(5)
 
 # while True:
-#     if recieved:
-#         break
-#     sender.send_pyobj(file)
-#     try:
-#         reciever.send_string('')
-#     except:
-#         try:
-#             recieved = reciever.recv()
-#         except:
-#             print("No reply")
+#     clientsocket, address = s.accept()
+#     print(f'Connection from {address} is established')
+#     clientsocket.send(bytes('Requesting stl file...', 'utf-8'))
 
+#Better Recieving
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 5556
+BUFFER_SIZE = stl.stl.BUFFER_SIZE
+SEPARATOR = "<SEPARATOR>"
 
-print("Recieved confimation!")
+#Creating socket
+s = socket.socket()
+s.bind((SERVER_HOST, SERVER_PORT))
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+client_socket, address = s.accept()
+print(f"[+] {address} is connected.")
 
+received = client_socket.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+filename = os.path.basename(filename)
+print(filesize)
+# filesize = int(filesize)
 
+recieved = client_socket.recv(BUFFER_SIZE)
+# print(sys.getsizeof(recieved))
+
+# progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open('out2.stl', "wb") as f:
+    while True:
+        bytes_read = client_socket.recv(BUFFER_SIZE)
+        if not bytes_read:    
+            break
+        f.write(bytes_read)
+client_socket.close()
+s.close()
+
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
+
+figure = pyplot.figure()
+axes = mplot3d.Axes3D(figure)
+
+my_mesh = stl.mesh.Mesh.from_file('out2.stl')
+axes.add_collection3d(mplot3d.art3d.Poly3DCollection(my_mesh.vextors))
+
+scale = my_mesh.points.flatten()
+axes.auto_scale_xyz(scale, scale, scale)
+pyplot.show()
--- processB.py
+++ processB.py
@@ -1,36 +1,64 @@
+from importlib.resources import open_binary
 from time import sleep
 import zmq
-import socket as netsocket
-from stl import mesh
+import socket
+import os
+import pickle
+import stl
 
 #Setting up ZeroMQ subscriber
 context = zmq.Context()
-sender = context.socket(zmq.REP)
-sender.bind('tcp://127.0.0.1:5555')
+reciever = context.socket(zmq.REP)
+reciever.bind('tcp://127.0.0.1:5555')
 
 #Recieves stl file and sends it back to process A
 incoming_file = None
 while not incoming_file:
-    incoming_file = sender.recv_pyobj()
+    incoming_file = reciever.recv_pyobj()
 
     sleep(1)
 
-    sender.send_pyobj(incoming_file)
+    reciever.send_pyobj(incoming_file)
 
 
 print("File Recieved")
 incoming_file.save('out.stl')
+reciever.close()
 
 #TODO: Parse File
 
 #Sending the file back
-# s = netsocket.socket()
-# host = netsocket.gethostname()
-# port = 5555
-# s.bind((host,port))
-# print(host)
-# s.listen(1)
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((socket.gethostname(), 5556))
 
-# print("Waiting for incoming connections...")
-# conn, addr = s.accept()
-# print("Connected")
+# while True:
+#     msg = s.recv(1024)
+#     print(msg.decode("utf-8"))
+
+
+#Better Sending
+BUFFER_SIZE = 4096
+SEPARATOR = "<SEPARATOR>"
+
+host = 'localhost'
+port = 5556
+
+filename = 'out.stl'
+filesize = os.path.getsize(filename)
+print(filesize)
+
+#creating client socket
+s = socket.socket()
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host,port))
+print('[+] Connected')
+s.send(f'{filename}{SEPARATOR}{filesize}'.encode())
+# print(type(filename))
+my_mesh = stl.mesh.Mesh.from_file('out.stl')
+# print(my_mesh)
+data_string = pickle.dumps(my_mesh)
+s.send(data_string)
+
+#sending the file
+s.close()
+print("File sent")
